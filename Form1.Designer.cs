21258000418e2f03edc31baa4d4a9bf183824834
Tom <thomasjh@gmail.com>
2025-11-02 10:14:10 +0100

    Commit of Main Program

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace BlogForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageButton = new System.Windows.Forms.Button();
+            this.captionLabel = new System.Windows.Forms.Label();
+            this.imageText = new System.Windows.Forms.TextBox();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.mainText = new System.Windows.Forms.RichTextBox();
+            this.markup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(574, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(123, 28);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(622, 20);
+            this.title.TabIndex = 2;
+            this.title.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Blog Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Short Description";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(124, 65);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(622, 51);
+            this.description.TabIndex = 5;
+            this.description.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 318);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 69);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // imageButton
+            // 
+            this.imageButton.Location = new System.Drawing.Point(15, 388);
+            this.imageButton.Name = "imageButton";
+            this.imageButton.Size = new System.Drawing.Size(100, 23);
+            this.imageButton.TabIndex = 8;
+            this.imageButton.Text = "Select Image";
+            this.imageButton.UseVisualStyleBackColor = true;
+            this.imageButton.Click += new System.EventHandler(this.imageButton_Click);
+            // 
+            // captionLabel
+            // 
+            this.captionLabel.AutoSize = true;
+            this.captionLabel.Location = new System.Drawing.Point(121, 393);
+            this.captionLabel.Name = "captionLabel";
+            this.captionLabel.Size = new System.Drawing.Size(75, 13);
+            this.captionLabel.TabIndex = 10;
+            this.captionLabel.Text = "Image Caption";
+            // 
+            // imageText
+            // 
+            this.imageText.Location = new System.Drawing.Point(202, 390);
+            this.imageText.Name = "imageText";
+            this.imageText.Size = new System.Drawing.Size(189, 20);
+            this.imageText.TabIndex = 11;
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Location = new System.Drawing.Point(12, 136);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(54, 13);
+            this.mainLabel.TabIndex = 12;
+            this.mainLabel.Text = "Main Text";
+            this.mainLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // mainText
+            // 
+            this.mainText.Location = new System.Drawing.Point(124, 133);
+            this.mainText.Name = "mainText";
+            this.mainText.Size = new System.Drawing.Size(622, 204);
+            this.mainText.TabIndex = 13;
+            this.mainText.Text = "";
+            // 
+            // markup
+            // 
+            this.markup.Location = new System.Drawing.Point(12, 164);
+            this.markup.Name = "markup";
+            this.markup.Size = new System.Drawing.Size(63, 20);
+            this.markup.TabIndex = 14;
+            this.markup.Text = "Markup";
+            this.markup.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.markup.UseVisualStyleBackColor = true;
+            this.markup.Click += new System.EventHandler(this.markup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.markup);
+            this.Controls.Add(this.mainText);
+            this.Controls.Add(this.mainLabel);
+            this.Controls.Add(this.imageText);
+            this.Controls.Add(this.captionLabel);
+            this.Controls.Add(this.imageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "New Blog Entry";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox description;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button imageButton;
+        private System.Windows.Forms.Label captionLabel;
+        private System.Windows.Forms.TextBox imageText;
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.RichTextBox mainText;
+        private System.Windows.Forms.Button markup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+
+
+namespace BlogForm
+{
+    public partial class Form1 : Form
+    {
+        // Declaring environment variable paths
+        private string imagePath;
+        private string blogPath;
+        private string repoPath;
+        public Form1()
+        {
+            InitializeComponent();
+            // Setting pictureBox settings
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Setting Environment Variables
+            foreach (var line in File.ReadAllLines(".env"))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#")) continue;
+
+                var parts = line.Split(new[] { '=' }, 2);
+                if (parts.Length == 2)
+                {
+                    Environment.SetEnvironmentVariable(parts[0].Trim(), parts[1].Trim());
+                }
+            }
+
+
+            this.imagePath = Environment.GetEnvironmentVariable("IMAGE_PATH");
+            this.blogPath = Environment.GetEnvironmentVariable("BLOG_PATH");
+            this.repoPath = Environment.GetEnvironmentVariable("REPO_PATH");
+        }
+
+        public class BlogContentItem
+        {
+            public string Type { get; set; }
+            public string Text { get; set; }
+            public string Src { get; set; }
+            public string Alt { get; set; }
+            public string Caption { get; set; }
+            public int? Level { get; set; }
+        }
+
+        public class BlogEntry
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Date { get; set; }
+            public List<Dictionary<string, object>> Content { get; set; }
+            public string Preview { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+            "Do you want to save?",
+            "Confirmation",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question
+        );
+
+            if (result == DialogResult.Yes)
+            {
+                // Save Image if one exists and we have a title.
+                if (pictureBox1.Image != null && title.Text != "")
+                {
+                    string fileName = title.Text + ".jpg";
+                    string imagePath = Path.Combine(this.imagePath, fileName);
+                    Console.WriteLine($"{imagePath}");
+                    pictureBox1.Image.Save(imagePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+
+                // Save Blog Post.
+                // We want to save: Description, Title, Date, Image Name
+                // Image name will be based on the title
+                string title_val = title.Text;
+                string description_val = description.Text;
+                string image_cap = imageText.Text;
+                string date_val = DateTime.Now.ToString("d MMMM yyyy");
+                string image_name = title_val + ".jpg";
+                string blogContent = mainText.Text;
+
+                // Process the blog content by spliting the entry's data up for JSON serialization
+
+                var splitContent = blogContent.Split(new[] { "$section" }, StringSplitOptions.RemoveEmptyEntries);
+                var jsonEntries = new List<Dictionary<string, object>>();
+
+                // Handle convert these JSON strings into actual JSON we want to use in the post.
+                foreach (var section in splitContent)
+                {
+                    var entry = new Dictionary<string, object>();
+
+                    if (section.Contains("$paragraph"))
+                    {
+                        entry["type"] = "paragraph";
+                        entry["text"] = ExtractValue(section, "$text");
+                    }
+                    else if (section.Contains("$image"))
+                    {
+                        entry["type"] = "image";
+                        entry["src"] = $"/{title_val}.jpg";
+                        entry["alt"] = ExtractValue(section, "$alt");
+                        entry["caption"] = image_cap;
+                    }
+                    else if (section.Contains("$heading"))
+                    {
+                        entry["type"] = "heading";
+                        entry["level"] = 2;
+                        entry["text"] = ExtractValue(section, "$text");
+                    }
+                    else if (section.Contains("$quote"))
+                    {
+                        entry["type"] = "quote";
+                        entry["text"] = ExtractValue(section, "$text");
+                    }
+
+                    jsonEntries.Add(entry);
+                }
+
+                // Convert
+                string json = JsonConvert.SerializeObject(jsonEntries, Formatting.Indented);
+
+                // Helper function to extract values
+                string ExtractValue(string input, string key)
+                {
+                    var match = Regex.Match(input, $@"{Regex.Escape(key)}\s+(.*?)(?=\s+\$|$)");
+                    return match.Success ? match.Groups[1].Value.Trim() : "";
+                };
+
+      
+            // This is the current existing data in the blog file:
+            string currentBlog = File.ReadAllText(this.blogPath);
+                int start = currentBlog.IndexOf("[");
+                int end = currentBlog.LastIndexOf("]");
+
+                if (start == -1 || end == -1 || end <= start)
+                    throw new InvalidOperationException("Could not locate JSON array brackets.");
+
+                string jsonArray = currentBlog.Substring(start, end - start + 1).Trim();
+                var entries = JsonConvert.DeserializeObject<List<BlogEntry>>(jsonArray);
+                // This is 1 indexed
+                int id_val = entries.Count + 1;
+
+                // Place all the new entry data together in one chunk
+                var newJson = new BlogEntry
+                {
+                    Id = id_val,
+                    Title = title_val,
+                    Date = date_val,
+                    Content = jsonEntries,
+                    Preview = description_val
+                };
+                // Add the new entry into the existing entries and serialize
+                entries.Add(newJson);
+                string updatedJson = JsonConvert.SerializeObject(entries, Formatting.Indented);
+
+                // Write to source file: 
+                string finalOutput = $"const entries = {updatedJson};\n\nexport default entries;";
+                File.WriteAllText(this.blogPath, finalOutput);
+
+                // Empty fields
+                description.Text = "";
+                imageText.Text = "";
+                mainText.Text = "";
+                title.Text = "";
+                pictureBox1.Image = null;
+
+                // Display Message
+
+                MessageBox.Show(
+                 $"Data submitted and saved for {title_val}",
+                 "Submission Successful",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+
+                // Push changes
+
+                RunGitCommand($"git add {image_name} blog.js");
+                RunGitCommand("git commit -m \"Add blog assets\"");
+                RunGitCommand("git push");
+
+
+            }
+            else
+            {
+                MessageBox.Show(
+                 $"Could not save data for {title.Text}",
+                 "Submission Not Successful",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+                );
+                return;
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            title.Text = "";
+            mainText.Text = "";
+            description.Text = "";
+            captionLabel.Text = "";
+            pictureBox1.Image = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void markup_Click(object sender, EventArgs e)
+        {
+            Form2 markupForm = new Form2();
+            markupForm.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void imageButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Display the image in a PictureBox named pictureBox1
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                }
+            }
+        }
+
+   
+        void RunGitCommand(string command)
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = $"/c {command}",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        WorkingDirectory = this.repoPath
+                    }
+                };
+
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                Console.WriteLine("Output:\n" + output);
+                if (!string.IsNullOrWhiteSpace(error))
+                    Console.WriteLine("Error:\n" + error);
+            }
+}
+}
